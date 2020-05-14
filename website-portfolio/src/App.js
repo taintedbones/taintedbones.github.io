273dd1e062fe137d5bf3dd6c1744d8f511112da9
taintedbones <kjust@csu.fullerton.edu>
2020-05-13 23:43:06 -0700

    Got app working on site! Also added some headers

--- website-portfolio/src/App.js
+++ website-portfolio/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar fixed="top" bg="dark" variant="dark">
+          <Navbar.Brand>Kierstyn Just</Navbar.Brand>
+          <DropdownButton className="Menu-Dropdown" variant="secondary" title="Menu">
+            <Dropdown.Item eventKey="1">About Me</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Experience</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Contact Me</Dropdown.Item>
+          </DropdownButton>
+        </Navbar>
+        <Container>
+          <h1>Kierstyn Just</h1>
+          <h4>Software Engineer</h4>
+        </Container>
       </header>
+      <Container>
+        <h3>About Me</h3>
+      </Container>
+      <Container>
+        <h3>Experience</h3>
+      </Container>
+      <Container>
+        <h3>Contact Me</h3>
+      </Container>
     </div>
   );
 }
