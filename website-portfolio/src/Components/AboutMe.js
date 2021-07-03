19847d9222f08daa57e44531ed860d99e36c94bc
taintedbones <kjust@csu.fullerton.edu>
2021-07-02 17:37:08 -0700

    Converted to  material ui

--- website-portfolio/src/Components/AboutMe.js
+++ website-portfolio/src/Components/AboutMe.js
@@ -2,35 +2,47 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
-import AboutPic from '../assets/about-photo.jpg';
 
-class AboutMe extends Component {
-    render() {
-        return (
-            <Row className="Body-Row">
-            <Container className="About-Me-Container" id="about">
-              <Row className="Body-Row">
-                <Col md={6} className="About-Photo-Col">
-                  <Image src={AboutPic} rounded className="About-Pic"></Image>
-                </Col>
-                <Col sm className="About-Paragraph-Col">
-                  <h3 className="Name-Text">About Me</h3>
-                  <br></br>
-                  <p>Hello, my name is Kierstyn Just. I'm currently an undergraduate 
-                    studying Computer Science at California State University, Fullerton.</p>
-                  <p>Before entering the world of programming, I was studying Fine Art and 
-                    earned Associate's Degrees in four different fields. Pairing 
-                    my art knowledge with a strong programming background, I am currently 
-                    focusing these skills on Game Development.</p>
-                  <p>When I'm not programming I enjoy traveling, watching horror movies, 
-                    playing video games, and brewery hopping!</p>
-                </Col>
-              </Row>
-            </Container>
-            </Row>
-        )
-    }
+import { Grid, makeStyles, Typography } from "@material-ui/core";
+
+import Container from "react-bootstrap/Container";
+import AboutPic from "../assets/about-photo.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "20px",
+  },
+  img: {
+    width:"100%",
+    maxWidth: "450px"
+  }
+}));
+
+function AboutMe() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container alignItems="flex-start" justify="center" spacing={3}>
+        <Grid item xs={12} sm={6} md={5}>
+          <img src={AboutPic} className={classes.img} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={5}>
+          <Typography variant="h4">About Me</Typography>
+          <Typography variant="body2">
+            Hello, my name is Kierstyn Just. I'm currently an undergraduate
+            studying Computer Science at California State University, Fullerton.
+            Before entering the world of programming, I was studying Fine Art
+            and earned Associate's Degrees in four different fields. Pairing my
+            art knowledge with a strong programming background, I am currently
+            focusing these skills on Game Development. When I'm not programming
+            I enjoy traveling, watching horror movies, playing video games, and
+            brewery hopping!
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default AboutMe;
