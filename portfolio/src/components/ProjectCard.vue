2218130f95801bb885a249aa9c3399e11a9988ff
taintedbones <kjust@csu.fullerton.edu>
2023-08-10 22:04:01 -0700

    added photos to proj cards

--- portfolio/src/components/ProjectCard.vue
+++ portfolio/src/components/ProjectCard.vue
@@ -1,22 +1,50 @@
 <script setup>
 const props = defineProps({
-    title: String,
-    text: String,
-    resources: [String],
-    repoUrl: String,
-    url: String
+  title: String,
+  text: String,
+  resources: [String],
+  repoUrl: String,
+  url: String,
+  img: String,
 });
 </script>
 
 <template>
-    project card
-    <v-card width="400">
-        <template v-slot:title>
-            {{ props.title }}
-        </template>
-
-        <template v-slot:text>
-            {{ props.text }}
-        </template>
-    </v-card>
+  <v-card class="proj-card" width="400" variant="tonal">
+    <v-img class="proj-img" :src="props.img" width="100%"></v-img>
+    <v-card-title>{{ props.title }}</v-card-title>
+    <v-card-subtitle
+      ><span class="text-overline" v-for="resource in resources"
+        >{{ resource
+        }}<span v-if="resource !== resources[resources.length - 1]"
+          >,
+        </span></span
+      ></v-card-subtitle
+    >
+    <v-card-text>{{ props.text }}</v-card-text>
+    <v-card-actions>
+      <v-btn variant="outlined" :href="props.repoUrl" target="_blank">
+        GitHub
+      </v-btn>
+      <v-btn
+        variant="outlined"
+        v-if="props.url"
+        :href="props.url"
+        target="_blank"
+      >
+        Link
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
+
+<style>
+.proj-img {
+  filter: grayscale(100%);
+  margin: 0px 10px;
+
+  &:hover {
+    filter: grayscale(0%);
+  }
+}
+</style>
--- portfolio/src/components/Projects.vue
+++ portfolio/src/components/Projects.vue
@@ -1,5 +1,8 @@
 <script setup>
 import ProjectCard from "./ProjectCard.vue";
+import CovidDashImg from "../assets/covid-dash-world-map.jpg";
+
+
 </script>
 
 <template>
@@ -9,5 +12,7 @@ import ProjectCard from "./ProjectCard.vue";
     text="A web application for tracking live COVID-19 data globally and in the US."
     repoUrl="https://github.com/taintedbones/covid-dash"
     url="https://taintedbones.github.io/covid-dash/"
+    :resources="['JavaScript', 'Material UI', 'Nivo', 'Axios']"
+    :img="CovidDashImg"
   />
 </template>
